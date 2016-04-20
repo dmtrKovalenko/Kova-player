8ccf532afd59a7eae11223fea7b8f01ffe7d420b
Quint <dmtr.kovalenko@outlook.com>
2016-04-20 20:05:48 +0300

    TEst

---
+++ Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Kova.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
